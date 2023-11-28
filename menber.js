818288b88b82cd5fb5dd1f042c06d0297ed52739
NaghostNext <93914664+NaghostNext@users.noreply.github.com>
2023-11-28 17:35:47 +0000

    Copilot second commit

---
+++ menber.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    var skills = document.getElementById('skills');
+    var skill = document.createElement('div');
+    skill.className = 'skill';
+    var skillName = document.createElement('input');
+    skillName.type = 'text';
+    skillName.className = 'skillName';
+    skill.appendChild(skillName);
+    var skillLevel = document.createElement('input');
+    skillLevel.type = 'number';
+    skillLevel.className = 'skillLevel';
+    skill.appendChild(skillLevel);
+    var skillButton = document.createElement('button');
+    skillButton.textContent = 'x';
+    skillButton.className = 'skillButton';
+    skillButton.onclick = function() {
+        skills.removeChild(skill);
+    };
+    skill.appendChild(skillButton);
+    skills.appendChild(skill);
+}
